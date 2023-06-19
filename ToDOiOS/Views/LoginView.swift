8928723f70d43678a466cd67f6918e5a1f5f9b51
IgorOdaryuk <odaryukigor@gmail.com>
2023-06-19 15:06:03 +0300

    Adding a Login Screen

--- ToDOiOS/Views/LoginView.swift
+++ ToDOiOS/Views/LoginView.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        VStack {
+            //Header
+            HeaderView()
+            
+            //LoginForm
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button {
+                    //Attempt log in
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        
+                        Text("Log In")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                }
+            }
+            //Create Account
+            VStack {
+                Text("New around here?")
+                Button("Create An Account") {
+                    //Show Registration
+                }
+            }
+            .padding(.bottom, 50)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
