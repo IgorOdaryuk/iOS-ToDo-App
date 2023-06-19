5344bd7d15f104f6ba66e95ebf5cce82c159521a
IgorOdaryuk <odaryukigor@gmail.com>
2023-06-19 15:31:14 +0300

    finishing the login screen

--- ToDOiOS/Views/LoginView.swift
+++ ToDOiOS/Views/LoginView.swift
@@ -14,42 +14,46 @@ struct LoginView: View {
     @State var password = ""
     
     var body: some View {
-        VStack {
-            //Header
-            HeaderView()
-            
-            //LoginForm
-            Form {
-                TextField("Email Address", text: $email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                SecureField("Email Address", text: $email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                Button {
-                    //Attempt log in
-                } label: {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(Color.blue)
-                        
-                        Text("Log In")
-                            .foregroundColor(Color.white)
-                            .bold()
-                    }
-                }
-            }
-            //Create Account
+        NavigationView {
             VStack {
-                Text("New around here?")
-                Button("Create An Account") {
-                    //Show Registration
+                //Header
+                HeaderView()
+                
+                //LoginForm
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button {
+                        //Attempt log in
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            
+                            Text("Log In")
+                                .foregroundColor(Color.white)
+                                .bold()
+                        }
+                    }
+                    .padding()
+                }
+                
+                //Create Account
+                VStack {
+                    Text("New around here?")
+                    
+                    NavigationLink("Create An Account", destination: RegisterView())
+                    }
+                        .padding(.bottom, 50)
+                
+                        Spacer()
                 }
             }
-            .padding(.bottom, 50)
-            
-            Spacer()
-        }
-    }
+       }
 }
 
 struct LoginView_Previews: PreviewProvider {
--- ToDOiOS/Views/RegisterView.swift
+++ ToDOiOS/Views/RegisterView.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text("Hello, World")
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
