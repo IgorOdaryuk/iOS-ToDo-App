2c656ed04c9a332c575a76370c1702f8a4d8c5ba
IgorOdaryuk <odaryukigor@gmail.com>
2023-06-19 19:31:37 +0300

    Adding user on basic level in the next branches more cooler view

--- ToDOiOS/Views/ToDoListView.swift
+++ ToDOiOS/Views/ToDoListView.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View{
+        Text("Welcome to your account")
+    }
+}
+
+struct ToDoListItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListView()
+    }
+}
