113e479333b447368aa1b92f2161a3700e8f3fdf
IgorOdaryuk <odaryukigor@gmail.com>
2023-06-20 10:11:22 +0300

    adding logic to the creating task and save button

--- ToDOiOS/ViewModels/NewItemViewViewModel.swift
+++ ToDOiOS/ViewModels/NewItemViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Igor Odaryuk on 19.06.2023.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -15,7 +16,32 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
+        //get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
         
+        //create a model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        //save model
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document("123")
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
